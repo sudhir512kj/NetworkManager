sudhir512kj/NetworkManager

commit 6c93e32212f9901e882d5a9ca221d037162bc740

libnm: don't use g_strsplit_set() to search string for invalid characters

libnm-core/nm-setting-team.c
@@ -168,7 +168,6 @@ nm_team_link_watcher_new_nsna_ping (gint init_wait,
                                     GError **error)
 {
 	NMTeamLinkWatcher *watcher;
-	gs_strfreev gchar **strv = NULL;
 	const char *val_fail = NULL;
 
 	if (!target_host) {
@@ -177,8 +176,7 @@ nm_team_link_watcher_new_nsna_ping (gint init_wait,
 		return NULL;
 	}
 
-	strv = g_strsplit_set (target_host, " \\/\t=\"\'", 0);
-	if (strv[1]) {
+	if (strpbrk (target_host, " \\/\t=\"\'")) {
 		g_set_error (error, NM_CONNECTION_ERROR, NM_CONNECTION_ERROR_FAILED,
 		             _("target-host '%s' contains invalid characters"), target_host);
 		return NULL;
@@ -236,7 +234,6 @@ nm_team_link_watcher_new_arp_ping (gint init_wait,
                                    GError **error)
 {
 	NMTeamLinkWatcher *watcher;
-	gs_strfreev gchar **strv = NULL;
 	const char *val_fail = NULL;
 
 	if (!target_host || !source_host) {
@@ -246,16 +243,13 @@ nm_team_link_watcher_new_arp_ping (gint init_wait,
 		return NULL;
 	}
 
-	strv = g_strsplit_set (target_host, " \\/\t=\"\'", 0);
-	if (strv[1]) {
+	if (strpbrk (target_host, " \\/\t=\"\'")) {
 		g_set_error (error, NM_CONNECTION_ERROR, NM_CONNECTION_ERROR_FAILED,
 		             _("target-host '%s' contains invalid characters"), target_host);
 		return NULL;
 	}
-	g_strfreev (strv);
 
-	strv = g_strsplit_set (source_host, " \\/\t=\"\'", 0);
-	if (strv[1]) {
+	if (strpbrk (target_host, " \\/\t=\"\'")) {
 		g_set_error (error, NM_CONNECTION_ERROR, NM_CONNECTION_ERROR_FAILED,
 		             _("source-host '%s' contains invalid characters"), source_host);
 		return NULL;

commit 4e1086634ff5d98f9f9489946a0d54ec5bdf36a7

libnm: fix checking argument in nm_team_link_watcher_new_arp_ping()
Found by coverity.

Fixes: 6c93e32212f9901e882d5a9ca221d037162bc740

libnm-core/nm-setting-team.c
@@ -249,7 +249,7 @@ nm_team_link_watcher_new_arp_ping (gint init_wait,
 		return NULL;
 	}
 
-	if (strpbrk (target_host, " \\/\t=\"\'")) {
+	if (strpbrk (source_host, " \\/\t=\"\'")) {
 		g_set_error (error, NM_CONNECTION_ERROR, NM_CONNECTION_ERROR_FAILED,
 		             _("source-host '%s' contains invalid characters"), source_host);
 		return NULL;